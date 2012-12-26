b43055a334aa6faf5a7dfb9b35d9aca8c107aa54
Hakki Kirmizi <hck@gopomo.com>
2012-12-27 05:42:33 +0800

    added support for pos tagging after segmentation.

--- run.sh
+++ run.sh
@@ -6,6 +6,9 @@ input_data_path=data/input
 # Stanford word segmenter path
 segmenter_path=lib/stanford-segmenter-2012-11-11
 
+# Stanford POS tagger path
+pos_tagger_path=lib/stanford-postagger-full-2012-11-11
+
 # opencc config file path
 opencc_config_file_path=/usr/lib/i386-linux-gnu/opencc/zhtw2zhcn_s.ini
 
@@ -17,6 +20,7 @@ output_path=data/output
 
 simplified_ext=.simplified
 segmented_ext=.segmented
+postag_ext=.pos_tagged
 cleaned_ext=.stopwords_cleaned
 freq_ext=.word_frequencies
 
@@ -40,6 +44,12 @@ for file in $input_data_path/* ; do
         -keepAllWhitespaces false \
         -loadClassifier $segmenter_path/data/ctb.gz \
         -serDictionary $segmenter_path/data/dict-chris6.ser.gz > $output_path/${filename}${segmented_ext}
+
+    echo '\nPOS tagging the words...\n\n'
+    java -mx2g -cp $pos_tagger_path/stanford-postagger.jar: edu.stanford.nlp.tagger.maxent.MaxentTagger \
+        -model $pos_tagger_path/models/chinese-distsim.tagger \
+        -outputFormat slashTags -tagSeparator \_ \
+        -textFile $output_path/${filename}${segmented_ext} > $output_path/${filename}${postag_ext}
 
     echo '\nCleaning stopwords...\n\n'
     # Clean stopwords
